bf3e7e67acf80de6ece457054c5de917144eb3a3
Lea45 <varzic.lea@gmail.com>
2022-11-20 23:15:30 +0100

    port

--- vjezba4/vjezba.js
+++ vjezba4/vjezba.js
@@ -1,39 +1,16 @@
 import express from 'express'
-//const express = require("express")
 import bodyParser from 'body-parser'
-import usersRoutes from "./routes/users.js"
-import uuid4 from "uuid4"
+
 
 const app = express()
-const PORT = 3000
+const port = 3000
 
 app.use(bodyParser.json())
-    var tempStorage = []
+var tempStorage = []
 
-app.get("/vratiSveKorisnike", (req, res) => {
+app.get("/korisnici", (req, res) => {
     console.log(tempStorage)
     res.send(tempStorage)
 })
 
-app.post("/dodajKorisnika", (req, res) => {
-    var data = res.body
-    console.log(data)
-    /*var {ime} = req.body
-    console.log(ime)
-    data.id = dataId
-    res.send(data)*/
-})
-
-app.patch("/urediKorisnika/:id", (req, res) =>{
-    var{id} = req.paramconsole.log(id)
-    res.send(id)
-})
-
-app.delete("/izbrisiKorisnika/:id", (req, res) => {
-    var {id} = req.params
-
-    tempStorage = tempStorage.filter(x => x.id == id)
-    res.send(tempStorage)
-})
-
-app.listen(port, () => console.log("Works on port ${port}"))
+app.listen(port, () => console.log('Works on port ${port}'))
